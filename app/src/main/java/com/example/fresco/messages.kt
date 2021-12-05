467efc12db80c0c2be922bda37c153e0d92595b2
Alex <stihinaleksander@yandex.ru>
2021-12-05 21:31:08 +0500

    add some changes

--- app/src/main/java/com/example/fresco/messages.kt
+++ app/src/main/java/com/example/fresco/messages.kt
@@ -1,4 +1,4 @@
-package com.example.fresco.fragments
+package com.example.fresco
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [home.newInstance] factory method to
+ * Use the [messages.newInstance] factory method to
  * create an instance of this fragment.
  */
-class home : Fragment() {
+class messages : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class home : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_messages, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class home : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment home.
+         * @return A new instance of fragment messages.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            home().apply {
+            messages().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
