850ec6c136fdf9fd03ea62703885782c586a6bdd
Alex <stihinaleksander@yandex.ru>
2021-12-05 22:05:45 +0500

    add some changes

--- app/src/main/java/com/example/fresco/MainActivity.kt
+++ app/src/main/java/com/example/fresco/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.my_nav)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragmentContainerView)
 
         bottomNavigationView.setupWithNavController(navController)
